3bf22c0473a96ec852ee4fdacfc2f474026cc279
werner glinka <werner@bussedesign.com>
2020-04-13 20:34:59 -0500

    added fix to gatsby-browser to prevent page jump when scrolled down

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -37,3 +37,15 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
     }
   });
 };
+
+const transitionDelay = 500;
+
+export const shouldUpdateScroll = ({ routerProps: { location }, getSavedScrollPosition }) => {
+  if (location.action === "PUSH") {
+    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay);
+  } else {
+    const savedPosition = getSavedScrollPosition(location);
+    window.setTimeout(() => window.scrollTo(...getSavedScrollPosition(savedPosition || [0, 0])), transitionDelay);
+  }
+  return false;
+};
