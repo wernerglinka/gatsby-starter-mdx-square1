c80c478634d8498701c375363cdeca7b7b8f727b
werner glinka <werner@bussedesign.com>
2020-06-29 16:59:37 -0500

    Added RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,5 +80,67 @@ module.exports = {
         },
       },
     },
+    {
+      // //////////////////////////////////////////////////////////////////////////////////
+      // RSS Feed with blog posts
+      // //////////////////////////////////////////////////////////////////////////////////
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteURL
+              }
+            }
+          }
+        `,
+        setup: () => ({
+          title: "Square1 Blog",
+          description: "Nullam quis risus eget urna mollis ornare vel eu leo.",
+        }),
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node, {
+                  title: edge.node.frontmatter.pageIntroduction.pageTitle,
+                  url: site.siteMetadata.siteURL + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteURL + edge.node.fields.slug,
+                  date: edge.node.frontmatter.date,
+                  description: edge.node.frontmatter.pageIntroduction.pageIntro,
+                  enclosure: {
+                    url: `https://res.cloudinary.com/glinkaco/image/upload/v1589395438/gatsby-square1/site-images/sample3_xhpwze.jpg`,
+                    type: "image/jpg",
+                  },
+                });
+              });
+            },
+            query: `
+              {
+                allMdx(filter: {fileAbsolutePath: {glob: "**/content/pages/blog/**/*.mdx"}, frontmatter: {template: {eq: "blog-post"}}}) {
+                  edges {
+                    node {
+                      frontmatter {
+                        pageIntroduction {
+                          pageTitle
+                          pageIntro
+                        }
+                        date
+                      }
+                      fields {
+                        slug
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/blog.xml",
+            title: "Square1 Blog Feed",
+          },
+        ],
+      },
+    },
   ],
 };
