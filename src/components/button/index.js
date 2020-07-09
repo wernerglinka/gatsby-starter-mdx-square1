538ced7f1544769e3cc8e3918037c3a3d2864693
werner glinka <werner@bussedesign.com>
2020-07-09 15:11:08 -0500

    refactored buttons with color choices

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -7,8 +7,19 @@ const ButtonLink = styled(Link)`
   display: block !important;
   line-height: 1 !important;
   padding: 10px 20px !important;
-  border: 2px solid #999;
-  background-color: #999;
+  border: 2px solid
+    ${props => {
+      if (props.buttonAttr === "primary") return props.theme.colors.primary;
+      if (props.buttonAttr === "secondary") return props.theme.colors.secondary;
+      if (props.buttonAttr === "contrast") return props.theme.colors.contrast;
+      return props.theme.colors.tertiary;
+    }};
+  background-color: ${props => {
+    if (props.buttonAttr === "primary") return props.theme.colors.primary;
+    if (props.buttonAttr === "secondary") return props.theme.colors.secondary;
+    if (props.buttonAttr === "contrast") return props.theme.colors.contrast;
+    return props.theme.colors.tertiary;
+  }};
   color: #fff;
   font-weight: 400;
   margin-left: 30px;
@@ -17,7 +28,12 @@ const ButtonLink = styled(Link)`
   &:hover {
     text-decoration: none;
     background-color: transparent;
-    color: #999;
+    color: ${props => {
+      if (props.buttonAttr === "primary") return props.theme.colors.primary;
+      if (props.buttonAttr === "secondary") return props.theme.colors.secondary;
+      if (props.buttonAttr === "contrast") return props.theme.colors.contrast;
+      return props.theme.colors.tertiary;
+    }};
   }
 `;
 
